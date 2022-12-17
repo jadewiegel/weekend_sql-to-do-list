6b4e667aeed64c85566fb8cb2fc2483b64f9b4e8
Jade Wiegel <jadewiegel@gmail.com>
2022-12-17 14:12:25 -0600

    input is being shown on consolelog and text field cleared

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,13 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jq loaded');
+    $('#toDoBtn').on('click', submitInput);
+}
+
+function submitInput(){
+    console.log('inside submitInput');
+    let textInput = $('#toDoText').val();
+    $('#toDoText').val('');
+    console.log('this is the text input', textInput);
+
 }
